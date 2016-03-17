68348a1540f5c22d6d9d80914cebada8f217b31a
Ossi Hanhinen <ossi.hanhinen@gmail.com>
2016-03-17 20:28:10 +0200

    Add step-1

---
+++ Main.elm
@@ -0,0 +1,29 @@
+module Main (..) where
+
+import Html exposing (..)
+
+
+{-
+You should have Elm installed on your computer.
+If you do, go ahead and try to compile this file with:
+
+    elm-make Main.elm
+
+If this was the first time you do this in this directory, Elm will install some
+necessary packages and then give an error about this code.
+
+Your job is to make this compile and say "Hello Elm" using the Html package
+already imported for you.
+
+The easiest way to serve the compiled app is to either use
+
+    elm-reactor
+
+or
+
+    elm-live Main.elm
+-}
+
+
+main =
+  0
